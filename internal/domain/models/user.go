12fdd9742f68e435e3ca07eb3f319c96dc80767d
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-07-03 10:04:19 +0700

    :money_with_wings: update model

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -2,10 +2,20 @@ package models
 
 import "time"
 
+type Role string
+
+const (
+	Admin Role = "ADMIN"
+	Users Role = "USER"
+)
+
 type User struct {
 	ID        string    `json:"id"`
-	Name      string    `json:"name"`
+	UserName  string    `json:"username"`
 	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Tel       string    `json:"tel"`
+	Role      Role      `json:"role"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
