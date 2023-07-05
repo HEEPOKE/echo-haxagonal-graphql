a03e118be9db6a96220601fbbd22102d27a6ad13
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-07-05 09:47:17 +0700

    :money_with_wings: refactor code unit test

--- internal/core/mocks/database_mock.go
+++ internal/core/mocks/database_mock.go
@@ -1,1 +1,41 @@
 package mocks
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/HEEPOKE/echo-haxagonal-graphql/pkg/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func SetupDatabase() (*mongo.Database, error) {
+	connectionString := config.Cfg.MONGO_URL
+	if connectionString == "" {
+		return nil, fmt.Errorf("MONGODB_CONNECTION_STRING environment variable not set")
+	}
+
+	clientOptions := options.Client().ApplyURI(connectionString)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to the database: %s", err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping the database: %s", err)
+	}
+
+	db := client.Database("test_database")
+
+	return db, nil
+}
+
+func TeardownDatabase(db *mongo.Database) error {
+	err := db.Drop(context.Background())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- internal/core/tests/repositories/user_repositories_test.go
+++ internal/core/tests/repositories/user_repositories_test.go
@@ -3,17 +3,15 @@ package repositories_test
 import (
 	"context"
 	"fmt"
-	"os"
 	"testing"
 	"time"
 
+	"github.com/HEEPOKE/echo-haxagonal-graphql/internal/core/mocks"
 	"github.com/HEEPOKE/echo-haxagonal-graphql/internal/domain/models"
 	"github.com/HEEPOKE/echo-haxagonal-graphql/internal/domain/repositories"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type UserRepositorySuite struct {
@@ -26,143 +24,78 @@ func TestUserRepositorySuite(t *testing.T) {
 }
 
 func (suite *UserRepositorySuite) SetupSuite() {
-	// Connect to the test MongoDB database
-	db, err := setupDatabase()
+	db, err := mocks.SetupDatabase()
 	if err != nil {
-		suite.FailNow(fmt.Sprintf("failed to set up database: %s", err))
+		suite.FailNow(fmt.Sprintf("failed to set up mongo database: %s", err))
 	}
 
 	suite.db = db
 }
 
 func (suite *UserRepositorySuite) TearDownSuite() {
-	// Clean up the test database
-	suite.db.Drop(context.Background())
+	mocks.TeardownDatabase(suite.db)
 }
 
-func setupDatabase() (*mongo.Database, error) {
-	// Get the MongoDB connection string from an environment variable
-	connectionString := os.Getenv("MONGODB_CONNECTION_STRING")
-	if connectionString == "" {
-		return nil, fmt.Errorf("MONGODB_CONNECTION_STRING environment variable not set")
-	}
-
-	// Connect to the MongoDB database
-	clientOptions := options.Client().ApplyURI(connectionString)
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to the database: %s", err)
-	}
-
-	// Ping the MongoDB server to check the connection
-	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to ping the database: %s", err)
-	}
-
-	// Get the test database from the MongoDB client
-	db := client.Database("test_database")
-
-	return db, nil
-}
-
-func TestGetAllUsers(t *testing.T) {
-	// Set up the database connection
-	db, err := setupDatabase()
-	if err != nil {
-		t.Fatalf("failed to set up database: %s", err)
-	}
-
-	// Create an instance of the UserRepository
+func (suite *UserRepositorySuite) TestGetAllUsers() {
 	userRepo := &repositories.UserRepository{
-		DB: db,
+		DB: suite.db,
 	}
 
-	// Call the method being tested
 	users, err := userRepo.GetAllUsers()
 
-	// Assert the expected results
-	assert.NoError(t, err)
-	assert.NotNil(t, users)
+	suite.NoError(err)
+	suite.NotNil(users)
 }
 
-func TestSaveUser(t *testing.T) {
-	// Set up the database connection
-	db, err := setupDatabase()
-	if err != nil {
-		t.Fatalf("failed to set up database: %s", err)
-	}
-
-	// Create an instance of the UserRepository
+func (suite *UserRepositorySuite) TestSaveUser() {
 	userRepo := &repositories.UserRepository{
-		DB: db,
+		DB: suite.db,
 	}
 
-	// Create a mock user
 	user := &models.User{
 		ID:        primitive.NewObjectID().Hex(),
 		Username:  "john_doe",
-		Email:     "john@example.com",
+		Email:     "johnq@example.com",
 		Tel:       "123456789",
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
 
-	// Call the method being tested
-	err = userRepo.SaveUser(user)
+	err := userRepo.SaveUser(user)
 
-	// Assert the expected results
-	assert.NoError(t, err)
+	suite.NoError(err)
 }
 
-func TestGetUserByID(t *testing.T) {
-	// Set up the database connection
-	db, err := setupDatabase()
-	if err != nil {
-		t.Fatalf("failed to set up database: %s", err)
-	}
-
-	// Create an instance of the UserRepository
+func (suite *UserRepositorySuite) TestGetUserByID() {
 	userRepo := &repositories.UserRepository{
-		DB: db,
+		DB: suite.db,
 	}
 
-	// Create a mock user and save it to the database
 	user := &models.User{
 		ID:        primitive.NewObjectID().Hex(),
 		Username:  "john_doe",
-		Email:     "john@example.com",
+		Email:     "johnaaaa@example.com",
 		Tel:       "123456789",
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
-	_, err = db.Collection("users").InsertOne(context.Background(), user)
+	_, err := suite.db.Collection("users").InsertOne(context.Background(), user)
 	if err != nil {
-		t.Fatalf("failed to insert user: %s", err)
+		suite.FailNow(fmt.Sprintf("failed to insert user: %s", err))
 	}
 
-	// Call the method being tested
 	result, err := userRepo.GetUserByID(user.ID)
 
-	// Assert the expected results
-	assert.NoError(t, err)
-	assert.NotNil(t, result)
-	assert.Equal(t, user.ID, result.ID)
+	suite.NoError(err)
+	suite.NotNil(result)
+	suite.Equal(user.ID, result.ID)
 }
 
-func TestUpdateUser(t *testing.T) {
-	// Set up the database connection
-	db, err := setupDatabase()
-	if err != nil {
-		t.Fatalf("failed to set up database: %s", err)
-	}
-
-	// Create an instance of the UserRepository
+func (suite *UserRepositorySuite) TestUpdateUser() {
 	userRepo := &repositories.UserRepository{
-		DB: db,
+		DB: suite.db,
 	}
 
-	// Create a mock user and save it to the database
 	user := &models.User{
 		ID:        primitive.NewObjectID().Hex(),
 		Username:  "john_doe",
@@ -171,50 +104,14 @@ func TestUpdateUser(t *testing.T) {
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
-	_, err = db.Collection("users").InsertOne(context.Background(), user)
+	_, err := suite.db.Collection("users").InsertOne(context.Background(), user)
 	if err != nil {
-		t.Fatalf("failed to insert user: %s", err)
+		suite.FailNow(fmt.Sprintf("failed to insert user: %s", err))
 	}
 
-	// Update the user
 	user.Username = "jane_doe"
 
-	// Call the method being tested
 	err = userRepo.UpdateUser(user)
 
-	// Assert the expected results
-	assert.NoError(t, err)
-}
-
-func TestDeleteUser(t *testing.T) {
-	// Set up the database connection
-	db, err := setupDatabase()
-	if err != nil {
-		t.Fatalf("failed to set up database: %s", err)
-	}
-
-	// Create an instance of the UserRepository
-	userRepo := &repositories.UserRepository{
-		DB: db,
-	}
-
-	// Create a mock user and save it to the database
-	user := &models.User{
-		ID:        primitive.NewObjectID().Hex(),
-		Username:  "john_doe",
-		Email:     "john@example.com",
-		Tel:       "123456789",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-	_, err = db.Collection("users").InsertOne(context.Background(), user)
-	if err != nil {
-		t.Fatalf("failed to insert user: %s", err)
-	}
-
-	// Call the method being tested
-	err = userRepo.DeleteUser(user.ID)
-
-	// Assert the expected results
-	assert.NoError(t, err)
+	suite.NoError(err)
 }
